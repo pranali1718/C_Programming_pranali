3453055367f3e5a38d654fe699005d00693c220e
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-06-09 17:46:05 +0530

    Create hello.c

---
+++ hello/hello.c
@@ -0,0 +1,14 @@
+/*
+in .c file have to add a user defined library file  and function body 
+*/
+
+#include "hello.h"
+
+int getfavouritenumber()
+{
+	return 3;
+}
+int getfavouritenumber1()
+{
+	return 4;
+}
