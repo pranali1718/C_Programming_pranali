9bdfc9af7930d9f72b0051267aaaf5eee69358d8
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-06-09 17:46:46 +0530

    Create hello.h

---
+++ hello/hello.h
@@ -0,0 +1,12 @@
+/*
+*in header(.h) file have to function declartion or function prototype and micros .
+and all header file
+*/
+
+#ifndef hello_H_INCLUDED
+#define hello_H_INCLUDED
+
+int getfavouritenumber();
+int getfavouritenumber1();
+
+#endif
